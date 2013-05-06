c682973ff79659a03fa628adfac49ab9758553dc
Greg Simon <gregsimon@chromium.org>
2013-05-06 15:22:35 -0700

    power button, change oscillator shape

--- background.js
+++ background.js
@@ -3,9 +3,9 @@ chrome.app.runtime.onLaunched.addListener(function() {
   // Tell your app what to launch and how.
   chrome.app.window.create('main.html', {
     width: 640,
-    height: 880,
+    height: 400,
     minWidth: 640,
-    minHeight: 480,
+    minHeight: 400,
     left: 0,
     top: 0,
     type: 'shell'
--- js/midi_test.js
+++ js/midi_test.js
@@ -2,11 +2,13 @@
 
 
 var requestButtonGN = document.getElementById("requestPermissionGN");
-var disconnectButton = document.getElementById("disconnect");
+//var disconnectButton = document.getElementById("disconnect");
 
 var audioContext;
 
 var freqMap;
+
+var synth_oscillator_type = 'square';
 
 var activeNotes = new Object();
 
@@ -23,14 +25,13 @@ function midiEvent(cin, m0, m1, m2) {
       			oscillator.noteOff && oscillator.noteOff(0);
       		}
 
-
       		// create a new oscillator
       		var oscillator = audioContext.createOscillator();
 			var gainNode = audioContext.createGainNode();  
 			oscillator.connect(gainNode);  
 			gainNode.connect(audioContext.destination); 
-			gainNode.gain.value = 1.0;
-			oscillator.type = 1;
+			gainNode.gain.value = m2 / 127.0;
+			oscillator.type = synth_oscillator_type;
       		oscillator.frequency.value = f;
       		oscillator.noteOn && oscillator.noteOn(0);
       		activeNotes[m1] = oscillator;
@@ -49,6 +50,9 @@ function midiEvent(cin, m0, m1, m2) {
         	console.log("OFF key="+m1);
         }
         break;
+
+        default:
+        	console.log("Unhandled MIDI event.");
     }
 }
 
@@ -98,8 +102,17 @@ requestButtonGN.addEventListener('click', function() {
 
 });
 
-disconnectButton.addEventListener('click', function() {
-	usbmidi_driver_disconnect();
+document.getElementById('oscSine').addEventListener('click', function() {
+	console.log("sine");
+	synth_oscillator_type = 'sine';
+});
+
+document.getElementById('oscSaw').addEventListener('click', function() {
+	synth_oscillator_type = 'sawtooth';
+});
+
+document.getElementById('oscSquare').addEventListener('click', function() {
+	synth_oscillator_type = 'square';
 });
 
 function keyToFreq(key) {
